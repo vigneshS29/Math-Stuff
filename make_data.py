fb26762f505ee06d4818f8fe6b9fe3166d909e4f
Vignesh Sathyaseelan <98504160+vigneshS29@users.noreply.github.com>
2023-06-18 18:27:41 -0400

    Add files via upload

---
+++ make_data.py
@@ -0,0 +1,25 @@
+import os,sys
+import numpy as np
+
+def main(argv):
+
+    f = lambda x : 1.0*np.sin(1*0.7*x) - 2.0*np.cos(1*0.7*x) + 1.0*np.sin(2*0.7*x) + 2.0*np.cos(2*0.7*x) \
+        - 3.0*np.sin(3.0*0.7*x) + 4.0*np.cos(3.0*0.7*x) + np.random.normal(scale=2.0,size = len(x))
+
+    # Generate f dataset
+    x = np.arange(0,10.01,0.05)
+    y = f(x)
+
+    write_data(x,y)
+
+    return
+
+def write_data(x,y):
+    with open("data_timeseries.txt",'w') as f:
+        f.write("{:<20s} {:<20s}\n".format("t-data","y-data"))        
+        for i in range(len(x)):
+            f.write("{:< 20.6f} {:< 20.6f}\n".format(x[i],y[i])) 
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
